2c1a408ba9d3cffa4b657e05eba246774d4cbe3c
Joab Selelya <joab.selelya@gmail.com>
2019-05-17 20:29:33 +0300

    Add project files.

---
+++ AgriInvestment.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AgriInvestment.WebUI.MvcApplication" Language="C#" %>
---
+++ AgriInvestment.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgriInvestment.WebUI.Startup))]
+namespace AgriInvestment.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
