80f52524a9048b83881a11465ad711cd33195077
Joab Selelya <joab.selelya@gmail.com>
2019-05-19 06:06:16 +0300

    Commit

--- AgriInvestment.WebUI/App_Start/BundleConfig.cs
+++ AgriInvestment.WebUI/App_Start/BundleConfig.cs
@@ -20,12 +20,12 @@ namespace AgriInvestment.WebUI
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/animate.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/animate.css",
+                        "~/Content/site.css"));
         }
     }
 }
